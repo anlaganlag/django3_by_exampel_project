f44259b8488ce35624d70986f40da76d3b17588d
gal <tanjhu@163.com>
2020-06-19 17:18:01 +0800

    加入tag功能

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
   def get_queryset(self):
@@ -19,6 +20,9 @@ class Post(models.Model):
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     objects = models.Manager() 
     published = PublishedManager() 
+    tags = TaggableManager()
+
+
     class Meta:
         ordering = ('-publish',)
     def __str__(self):
